738f0e2f7ffcb0608618ffe47e9695917eb944b5
gribdesbois <gribdesbois@gmail.com>
2021-11-30 23:35:56 +0100

    read component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,25 @@
 import './App.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Create from './components/Create'
+import Read from './components/Read'
+import Update from './components/Update'
+/* import Delete from './components/Delete' */
 
 function App() {
   return (
-    <div className="main">
-      <h2 className='main-header'>React Crud Operations</h2>
-      <div>
-        <Create />
+    <Router>
+      <div className="main">
+        <h2 className='main-header'>React Crud Operations</h2>
+        <div>
+          <Route exact path='/create' component={Create} />
+        </div>
+        <div style={{ marginTop: 20 }} >
+          <Route exact path='/read' component={Read} />
+        </div>
+
+        <Route path='/update' component={Update} />
       </div>
-    </div>
+    </Router>
   )
 }
 
--- frontend/src/components/Read.js
+++ frontend/src/components/Read.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import axios from 'axios'
+import _ from 'dotenv/config'
+import React, { useEffect, useState } from 'react'
+import { Table } from 'semantic-ui-react'
+
+const { REACT_APP_API_URL } = process.env
 
 export default function Read() {
+  const [APIData, setAPIData] = useState([])
+
+  useEffect(() => {
+    axios.get(REACT_APP_API_URL)
+      .then((response) => {
+        setAPIData(response.data)
+      })
+  }, [])
+
   return (
     <div>
+      <Table singleLine>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>First Name</Table.HeaderCell>
+            <Table.HeaderCell>Last Name</Table.HeaderCell>
+            <Table.HeaderCell>Checked</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
 
+        <Table.Body>
+          {APIData.map((data) => (
+            <Table.Row>
+              <Table.Cell>{data.firstName}</Table.Cell>
+              <Table.Cell>{data.lastName}</Table.Cell>
+              <Table.Cell>{data.checkbox ? 'Checked' : 'Unchecked'}</Table.Cell>
+            </Table.Row>
+          ))
+          }
+
+        </Table.Body>
+      </Table>
     </div>
   )
 }
